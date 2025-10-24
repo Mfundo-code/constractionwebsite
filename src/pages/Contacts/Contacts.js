9d5fbb924ffe69e2de54e0ac610f2a7d62816ab4
Mfundo <mfundoknox@gmail.com>
2025-10-24 14:33:59 +0200

    contatcs files

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ContactHero from "./ContactsComponents/ContactHero";
 import ContactForm from "./ContactsComponents/ContactForm";
-import ContactDetails from "./ContactsComponents/ContactDetails";
 import MapSection from "./ContactsComponents/MapSection";
-import OfficeLocations from "./ContactsComponents/OfficeLocations";
-import SocialLinks from "./ContactsComponents/SocialLinks";
-import ContactCTA from "./ContactsComponents/ContactCTA";
+
 
 const Contacts = () => {
   return (
@@ -13,11 +10,7 @@ const Contacts = () => {
       <div>hey i'm Contacts page</div>
       <ContactHero />
       <ContactForm />
-      <ContactDetails />
       <MapSection />
-      <OfficeLocations />
-      <SocialLinks />
-      <ContactCTA />
     </main>
   );
 };
